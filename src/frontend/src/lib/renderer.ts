fa504cbed507064b8a70d5ca6018aee04da8b85e
Quine Global <quine@fastmail.com>
2025-12-30 03:48:30 -0500

    add fela

---
+++ src/frontend/src/lib/renderer.ts
@@ -0,0 +1,29 @@
+import { createRenderer } from 'fela'
+import embedded from 'fela-plugin-embedded'
+import prefixer from 'fela-plugin-prefixer'
+import fallbackValue from 'fela-plugin-fallback-value'
+import unit from 'fela-plugin-unit'
+import perf from 'fela-perf'
+import beautifier from 'fela-beautifier'
+import sortMediaQueryMobileFirst from 'fela-sort-media-query-mobile-first'
+
+export default () => {
+  const renderer = createRenderer({
+    plugins: [embedded(), unit(), prefixer(), fallbackValue()],
+    enhancers: [perf(), beautifier(), sortMediaQueryMobileFirst()],
+  })
+
+  renderer.renderStatic(
+    {
+      width: '100%',
+      height: '100%',
+      margin: 0,
+      padding: 0,
+      fontFamily: 'Lato',
+    },
+    'html,body,#app'
+  )
+
+  renderer.renderStatic({ display: 'flex' }, 'div')
+  return renderer
+}
--- src/frontend/src/routes/index.tsx
+++ src/frontend/src/routes/index.tsx
@@ -1,16 +1,38 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { useSession } from '../lib/auth-client'
 import { Button } from '@base-ui/react/button';
+import { RendererProvider, createComponent } from 'react-fela'
+
+import createRenderer from '../lib/renderer'
+
+const renderer = createRenderer()
 
 export const Route = createFileRoute('/')({
   component: Index,
 })
 
+const section = () => ({
+  margin: '0 auto',
+  fontSize: '1rem',
+  display: 'block',
+  width: '30.5rem',
+  height: '30.5rem',
+  background: 'blue'
+})
+
+const LoginBox = createComponent(section)
+
+
 function Index() {
   return (
     <html>
       <body>
-        <MyApp />
+        <RendererProvider renderer={renderer}>
+          <MyApp />
+          <LoginBox>
+            Hello styles
+          </LoginBox>
+        </RendererProvider>
       </body>
     </html>
   )
@@ -28,11 +50,12 @@ function MyApp() {
   }
 
   return (
-      <div>
-        <Button native={false}>Let's go</Button>
+    <div>
+      <Button native={false}>Let's go</Button>
 
-        <h3>Welcome Home!</h3>
-        {session && <div>Hello {session.user?.name ?? `${session.user}`}</div>}
-      </div>
+      <h3>Welcome Home!</h3>
+
+      {session && <div>Hello {session.user?.name ?? `${session.user}`}</div>}
+    </div>
   )
 }
